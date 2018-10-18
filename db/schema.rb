bdc9e193bfb1eaac5bee3edcb512ecf016540f39
deveye <malvy.duo@gmail.com>
2018-10-18 21:21:32 +0300

    Skills via model-generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_155027) do
+ActiveRecord::Schema.define(version: 2018_10_18_180401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_10_17_155027) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "skill_percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
