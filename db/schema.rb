b743b953981790f54f0c182e2880634b3f433489
deveye <malvy.duo@gmail.com>
2018-10-20 18:09:40 +0300

    Added helper class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_135110) do
+ActiveRecord::Schema.define(version: 2018_10_20_145942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2018_10_20_135110) do
     t.integer "skill_percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
