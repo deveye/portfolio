d35c1b0b60321cdca34ff4d539493b1eb45b407a
deveye <malvy.duo@gmail.com>
2018-10-21 22:09:48 +0300

    Implemented first application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+
+  def copyright_footer
+    EekkViewTool::Renderer.copyright 'Kristo', 'All rights reserved'
+  end
 end
