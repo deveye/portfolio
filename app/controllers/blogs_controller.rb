f6f0dee54e78d22425c4af06acdd7f454ce7f7cb
deveye <malvy.duo@gmail.com>
2018-10-21 12:55:27 +0300

    Added custom stylesheet for blogs and pportfolios

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  layout "blog" ## Stylesheet for blog layout
 
   # GET /blogs
   # GET /blogs.json
